4e8c56bb729dccad8381f289e08f33f4312df601
Trishul Wadhwa <fiestyknight404@gmail.com>
2022-02-17 10:04:50 +0530

    auth controllers done

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,9 +37,11 @@ const login = async(req, res) => {
 }
 
 const securityLogin = async(req, res) => {
-    const {username, security_question, security_answer} = req.body;
-
-    const user = await User.findOne({user:username})
+    const {name, security_question, security_answer} = req.body;
+    if(!name || !security_question || !security_answer) {
+        throw new BadRequestError('Username, Security Question and Answer');
+    }
+    const user = await User.findOne({name})
 
     if(!user) {
         throw new UnauthenticatedError('User does not exist');
